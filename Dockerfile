53865456c6501e161d4a44e2848da3bf77456cad
Antônio M. Quadros Filho <antonio@4.events>
2024-06-20 21:59:05 -0300

    feature: .htaccess

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 # Usar a imagem oficial do PHP com Apache
 FROM php:7.4-apache
 
+# ENV MYSQL_PASSWORD=${MYSQL_PASSWORD}
+
 # Habilitar módulos do Apache e do PHP necessários
 RUN a2enmod rewrite
 
@@ -8,6 +10,8 @@ RUN a2enmod rewrite
 RUN docker-php-ext-install pdo pdo_mysql
 
 # Copiar o arquivo de configuração do Virtual Host para o Apache
+# RUN rm /etc/apache2/sites-available/000-default.conf
+
 COPY ./apache/sites-available/vhost.conf /etc/apache2/sites-available/000-default.conf
 
 # Expor a porta 80 para acesso externo
@@ -15,3 +19,6 @@ EXPOSE 80
 
 # Copiar o código do projeto para o diretório raiz do Apache
 COPY ./src/ /var/www/html/
+
+# Reiniciar o serviço do Apache após fazer mudanças
+CMD apachectl -D FOREGROUND
--- src/index.php
+++ src/index.php
@@ -1,3 +1,46 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-echo phpinfo();
+<?php if(!empty($_GET['uri'])) { ?>
+    <p>Está tentando acessar a uri [<?php echo $_GET['uri']; ?>]?</p>
+<?php } ?>
+
+<ul>
+    <li>
+        <a href="/">Home</a>
+    </li>
+    <li>
+        <a href="/menu1">Menu 1</a>
+    </li>
+    <li>
+        <a href="/menu2">Menu 2</a>
+    </li>
+</ul>
+
+<h1>Environment variables</h1>
+
+<h2>Fixo no vhost.conf</h2>
+
+<?php echo "\$_SERVER['MYSQL_USER']: {$_SERVER['MYSQL_USER']}";  ?>
+
+<h2>Dinâmico no vhost.conf</h2>
+
+<p>Esta variável está sendo repassada pelo vhost.conf, mas foi definida no docker-compose.yml:</p>
+
+<?php echo "\$_SERVER['MYSQL_PASSWORD']: {$_SERVER['MYSQL_PASSWORD']}";  ?>
+
+<h2>docker-compose.yml</h2>
+
+<?php 
+    $mysqlPassword=getenv('MYSQL_PASSWORD');
+    echo "getenv('MYSQL_PASSWORD'): {$mysqlPassword}";
+?>
+
+</body>
+</html>
